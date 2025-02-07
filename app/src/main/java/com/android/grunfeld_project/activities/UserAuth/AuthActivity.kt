fd312dc9fc94c5a63aecec383d14c4108e61c511
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-02-07 17:38:34 +0530

    prog: Auth Activity UI

--- app/src/main/java/com/android/grunfeld_project/activities/UserAuth/AuthActivity.kt
+++ app/src/main/java/com/android/grunfeld_project/activities/UserAuth/AuthActivity.kt
@@ -16,10 +16,12 @@ class AuthActivity : AppCompatActivity() ***REMOVED***
     override fun onCreate(savedInstanceState: Bundle?) ***REMOVED***
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+//        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) ***REMOVED*** v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 ***REMOVED***
+        window.statusBarColor = getColor(R.color.black)
 ***REMOVED***
 ***REMOVED***
